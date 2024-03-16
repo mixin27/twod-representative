13d5c86d45aa1f1fc49079e488dd37d2ae59fb67
mixin27 <kyawzayartun.dev@gmail.com>
2024-03-16 11:34:06 +0630

    :bug: fix(server): change app to server

--- server/src/index.ts
+++ server/src/index.ts
@@ -37,6 +37,6 @@ io.on("connection", (socket) => {
 
 app.use(errorHandler);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`🚀 Twod Representative API server is listening on port ${PORT}`);
 });
